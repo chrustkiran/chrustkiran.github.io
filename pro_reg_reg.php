f54352e09cd83009ccba00b85837898055511a5c
chrustkiran <christkiran.15@cse.mrt.ac.lk>
2017-05-11 21:18:26 +0530

    Update pro_reg_reg.php

--- pro_reg_reg.php
+++ pro_reg_reg.php
@@ -1,7 +1,7 @@
 <?php
 
 //$con = mysqli_connect("localhost","id1577001_userss","kiran123","id1577001_users") or die($con);
-$con = mysqli_connect("localhost","root","","user") or die($con);
+$con = mysqli_connect("sql9.freesqldatabase.com","sql9173811","hsR16bldP1","sql9173811") or die($con);
 //$i=intval(mysqli_real_escape_string($con,$_POST['id']));
 $u=mysqli_real_escape_string($con,$_POST['username']);
 $p=mysqli_real_escape_string($con,(string)($_POST['password']));

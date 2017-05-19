8302545f9e73a3719beea362ac50246d27fe8484
chrustkiran <christkiran.15@cse.mrt.ac.lk>
2017-05-19 21:18:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 				<input type="submit" name = "login" id="btn" value="Login">
 		</form>
 			    <form action="process_reg.php" method="POST">
-					<input type="submit" " value="Register" id="btn1">
+					<input type="submit"  value="Register" id="btn1">
 				</form>
 			
 			</p>
